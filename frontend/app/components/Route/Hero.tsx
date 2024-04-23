2316911a3aa848cb8432d2362b86ac6c6607b036
Sebona Ifa <sebona@sebona.localdomain>
2024-04-23 14:05:52 +0300

    switch

--- frontend/app/components/Route/Hero.tsx
+++ frontend/app/components/Route/Hero.tsx
@@ -8,7 +8,7 @@ type Props = {};
 const Hero: FC<Props> = (props) => {
   return (
     <div className="w-full 1000px:flex items-center">
-      <div className="absolute top-[100px] left-[35px] 1000px:top-[unset] 1100px:h-[400px] 1100px:w-[400px] h-[1vh] w-[1vh] hero_animation relative ">
+      <div className="absolute top-[100px] left-[35px] 1000px:top-[unset] 1100px:h-[400px] 1100px:w-[400px]  hero_animation relative ">
         <div className="1000px:h-[100%] flex 10px:min-h-screen ">
           <Image
             src={require("../../../public/assets/lmsBanner.png")}
@@ -18,12 +18,12 @@ const Hero: FC<Props> = (props) => {
         </div>
         </div>
 
-        <div className="1200px:w-[50%] left-[305px] 100px:mt-[0px] text-center 10px:text-left mt-[150px]">
-          <h2 className="dark:text-white text-[#000000c7] text-[30px] px-43 1000px:text-[50px] font-[600] font-Josefin py-2 1000px:leading-[75px] 10px:w-[600%]">
+        <div className="1000px:w-[50%] flex flex-col items-center 1000px:mt-[0px] text-center 10px:text-left mt-[150px] hero_text">
+          <h2 className="dark:text-white text-[#000000c7] text-[30px] px-3 w-full 1000px:text-[35px] font-[600] font-Josefin py-2 1000px:leading-[75px] 1500px:w-[60%]">
             Improve Your Online Learning Experience Better Instantly
           </h2>
           <br />
-          <p className="dark:text-[#edfff4] text-[#000000ac] font-Josefin font-[600] text-[18px] 1500px:!w-[55%] 1100px:!w-[78%]">
+          <p className="dark:text-[#edfff4] text-[#000000ac] font-Josefin font-[600] text-[14px] 1500px:!w-[55%] 1100px:!w-[78%]">
             We have 40k+ Online courses & 500K+ Online registered student. Find
             your desired Courses from them.
           </p>
@@ -33,7 +33,7 @@ const Hero: FC<Props> = (props) => {
             <input
               type="search"
               placeholder="Search Courses..."
-              className="bg-transparent border dark:border-none dark:bg-[#575757] dark:placeholder:text-[#ffffffdd] rounded-[15px] p-2 w-full h-full outline-none text-black dark:text-[#ffffffe6] text-[20px] font-[500] font-Josefin"
+              className="bg-transparent border dark:border-none dark:bg-[#575757] dark:placeholder:text-[#ffffffdd] rounded-[5px] p-2 w-full h-full outline-none text-black dark:text-[#ffffffe6] text-[20px] font-[500] font-Josefin"
             />
             <div className="absolute flex items-center justify-center w-[50px] cursor-pointer h-[50px] right-0 top-0 bg-[#39c1f3] rounded-r-[5px]">
               <BiSearch className="text-white" size={30} />
@@ -57,7 +57,7 @@ const Hero: FC<Props> = (props) => {
               alt=""
               className="rounded-full ml-[-20px]"
             />
-            <p className="font-Josefin dark:text-[#edfff4] text-[#000000b3] 1000px:pl-3 text-[18px] font-[600]">
+            <p className="font-Josefin dark:text-[#edfff4] text-[#000000b3] 1000px:pl-3 text-[14px] font-[600]">
               500K+ People already trusted us.{" "}
               <Link
                 href={"/courses"}
@@ -68,7 +68,7 @@ const Hero: FC<Props> = (props) => {
             </p>
           </div>
           <br />
-      </div>
+        </div>
     </div>
   );
 };
